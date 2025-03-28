cd570b220cffbff7681ffa0106f2a73a6f4823fc
Ali-Hadi-Saker <alihady530@gmail.com>
2025-03-29 00:58:21 +0200

    initialized register page

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,14 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './Styles/App.css';
 
 function App() {
   return (
     <div>
-      
+      <BrowserRouter >
+        <Routes>
+          <Route ></Route>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
