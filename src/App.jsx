f2094ed41df4184e1835e971b68ddff1595cc4d6
Ali-Hadi-Saker <alihady530@gmail.com>
2025-03-29 01:09:02 +0200

    added Login route

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './Styles/App.css';
 import Register from './Pages/Register';
+import Login from './Pages/Login';
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <BrowserRouter >
         <Routes>
           <Route path='/' element={<Register/>}></Route>
+          <Route path='/Login' element={<Login/>}></Route>
         </Routes>
       </BrowserRouter>
     </div>
