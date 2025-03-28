31b68e44703e90efee34486913ff2a384c2aff7a
Ali-Hadi-Saker <alihady530@gmail.com>
2025-03-29 01:27:29 +0200

    added form for register page

--- src/Pages/Register/index.jsx
+++ src/Pages/Register/index.jsx
@@ -1,7 +1,17 @@
 const Register = () => {
     return(
-        <div>
+        <div className="flex column page center">
             <h1>Register page</h1>
+            <div className="flex column "></div>
+            <input type="text" placeholder="First Name"/>
+            <input type="text" placeholder="Last Name"/>
+            <input type="text" placeholder="Email Adress"/>
+            <input type="text" placeholder="Disability Card Code"/>
+            <input type="text" placeholder="Disability"/>
+            <input type="text" placeholder="Job Interest"/>
+            <input type="password" placeholder="Password"/>
+            <input type="password" placeholder="Confirm Password"/>
+            <button>Submit</button>
         </div>
     )
 }
