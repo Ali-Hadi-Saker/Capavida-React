51536a104fec66e379be1c09a8a3cffa38a8b405
Ali-Hadi-Saker <alihady530@gmail.com>
2025-03-30 01:42:37 +0300

    added onClick function to menu icon

--- src/Components/Navbar/index.jsx
+++ src/Components/Navbar/index.jsx
@@ -8,7 +8,7 @@ const Navbar = () => {
     return(
         <nav className='navbar flex'>
                 <ul className='nav-list flex'>
-                    <li><i className="fa fa-bars" aria-hidden="true"></i></li>
+                    <li><i className="fa fa-bars" onClick={toggleSideBar}></i></li>
                     <li>Dashboard</li>
                     <li><i className="fa fa-bell" aria-hidden="true"></i></li>
                 </ul>
