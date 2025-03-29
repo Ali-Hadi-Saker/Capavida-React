ceb68304dda4319fd80a0c0e36703b678b38d9f9
Ali-Hadi-Saker <alihady530@gmail.com>
2025-03-29 03:16:52 +0200

    added style for register form

--- src/Pages/Register/index.jsx
+++ src/Pages/Register/index.jsx
@@ -1,17 +1,20 @@
+import './style.css';
+
 const Register = () => {
     return(
         <div className="flex column page center">
             <h1>Register page</h1>
-            <div className="flex column "></div>
-            <input type="text" placeholder="First Name"/>
-            <input type="text" placeholder="Last Name"/>
-            <input type="text" placeholder="Email Adress"/>
-            <input type="text" placeholder="Disability Card Code"/>
-            <input type="text" placeholder="Disability"/>
-            <input type="text" placeholder="Job Interest"/>
-            <input type="password" placeholder="Password"/>
-            <input type="password" placeholder="Confirm Password"/>
-            <button>Submit</button>
+            <div className="flex column center auth-box">
+                <input type="text" placeholder="First Name"/>
+                <input type="text" placeholder="Last Name"/>
+                <input type="text" placeholder="Email Adress"/>
+                <input type="text" placeholder="Disability Card Code"/>
+                <input type="text" placeholder="Disability"/>
+                <input type="text" placeholder="Job Interest"/>
+                <input type="password" placeholder="Password"/>
+                <input type="password" placeholder="Confirm Password"/>
+                <button>Submit</button>
+            </div>
         </div>
     )
 }
