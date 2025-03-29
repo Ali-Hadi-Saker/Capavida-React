02231dc0564b4c87d218fd87705bd243a73a7165
Ali-Hadi-Saker <alihady530@gmail.com>
2025-03-29 16:00:26 +0200

    created navbar component

---
+++ src/Components/Navbar/index.jsx
@@ -0,0 +1,15 @@
+import './style.css';
+
+const Navbar = () => {
+    return(
+        <nav className='flex navbar'>
+                <ul>
+                    <li>Menu</li>
+                    <li>Dashboard</li>
+                    <li>Notification</li>
+                </ul>
+            </nav>
+    );
+};
+
+export default Navbar;
--- src/Pages/Home/index.jsx
+++ src/Pages/Home/index.jsx
@@ -1,16 +1,11 @@
+import Navbar from '../../Components/Navbar';
 import './style.css';
 
 const Home = () => {
     return(
         <div>
             <h1>Home page</h1>
-            <nav className='flex navbar'>
-                <ul>
-                    <li>Menu</li>
-                    <li>Dashboard</li>
-                    <li>Notification</li>
-                </ul>
-            </nav>
+            <Navbar/>
         </div>
     )
 }
