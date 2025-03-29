646de99dfdbd0bad9ed8b6e7cd357f07d337a494
Ali-Hadi-Saker <alihady530@gmail.com>
2025-03-29 03:46:58 +0200

    added login page form

--- src/Pages/Login/index.jsx
+++ src/Pages/Login/index.jsx
@@ -1,7 +1,18 @@
+import { Link } from "react-router-dom";
+import './style.css';
+
 const Login = () => {
     return(
-        <div>
-            <h1>Login page</h1>
+        <div className="flex column page center">
+            <h1 className='green-text'>Intern Sign-up</h1>
+            <div className="flex column center auth-box">
+                <input type="text" placeholder="Email Adress"/>
+                <input type="password" placeholder="Password"/>
+                <button>Submit</button>
+                <p className="login-link black-text">dont have an account{"  "}
+                <Link to="/">Sign-up here</Link>
+                </p>
+            </div>
         </div>
     )
 }
