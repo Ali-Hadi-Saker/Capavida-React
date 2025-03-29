50f72dbc8b66a590bd9154768b4d71bac277b63f
Ali-Hadi-Saker <alihady530@gmail.com>
2025-03-29 03:33:06 +0200

    updated register style

--- src/Pages/Register/index.jsx
+++ src/Pages/Register/index.jsx
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 const Register = () => {
     return(
         <div className="flex column page center">
-            <h1>Register page</h1>
+            <h1 className='green-text'>Intern Sign-up</h1>
             <div className="flex column center auth-box">
                 <input type="text" placeholder="First Name"/>
                 <input type="text" placeholder="Last Name"/>
@@ -15,7 +15,7 @@ const Register = () => {
                 <input type="password" placeholder="Password"/>
                 <input type="password" placeholder="Confirm Password"/>
                 <button>Submit</button>
-                <p className="login-link">already have an account{"  "}
+                <p className="login-link black-text">already have an account{"  "}
                 <Link to="/login">login here</Link>
                 </p>
             </div>
