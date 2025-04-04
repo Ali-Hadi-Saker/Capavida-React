147f353f77a5ccbd74b23258fc050d60f8762bfb
Ali-Hadi-Saker <alihady530@gmail.com>
2025-04-05 02:30:10 +0300

    updated navbar style

--- src/Components/Navbar/index.jsx
+++ src/Components/Navbar/index.jsx
@@ -1,20 +1,29 @@
 import Sidebar from '../Sidebar';
 import './style.css';
 import { useState } from 'react';
-
+import { Link } from 'react-router-dom';
 
 const Navbar = () => {
     const [isOpen, setIsOpen] = useState(false);
     const toggleSidebar = () => setIsOpen(!isOpen);
-    return(
-        <nav className='navbar flex'>
-                <ul className='nav-list flex'>
-                    <li><i className="fa fa-bars" onClick={toggleSidebar}></i></li>
-                    <li>Dashboard</li>
-                    <li><i className="fa fa-bell" aria-hidden="true"></i></li>
-                </ul>
-                <Sidebar isOpen={isOpen} toggleSidebar={toggleSidebar}/>
-            </nav>
+    
+    return (
+        <nav className='navbar'>
+            <div className='navbar-content'>
+                <div className='navbar-left'>
+                    <button className="menu-button" onClick={toggleSidebar}>
+                        <i className="fa fa-bars"></i>
+                    </button>
+                    <Link to="/" className="nav-brand">CapaVida</Link>
+                </div>
+                <div className='navbar-right'>
+                    <button className="nav-button">
+                        <i className="fa fa-bell"></i>
+                    </button>
+                </div>
+            </div>
+            <Sidebar isOpen={isOpen} toggleSidebar={toggleSidebar}/>
+        </nav>
     );
 };
 
