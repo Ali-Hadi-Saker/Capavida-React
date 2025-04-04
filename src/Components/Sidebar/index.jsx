8e52eb77c76818c2df148f0033904e895a209e69
Ali-Hadi-Saker <alihady530@gmail.com>
2025-04-05 02:35:33 +0300

    updated sidebar style

--- src/Components/Sidebar/index.jsx
+++ src/Components/Sidebar/index.jsx
@@ -1,23 +1,58 @@
 import { Link } from 'react-router-dom';
 import './style.css';
 
-
-const Sidebar = ({isOpen, toggleSidebar})=> {
-    return(
+const Sidebar = ({isOpen, toggleSidebar}) => {
+    return (
         <>
+            <div className={`sidebar-overlay ${isOpen ? "open" : ""}`} onClick={toggleSidebar}></div>
             <div className={`sidebar ${isOpen ? "open" : ""}`}>
-            <button className='close-btn' onClick={toggleSidebar}>X</button>
-            <ul className='sidebar-menu'>
-                <li><Link to="/home" onClick={toggleSidebar} className="sidebar-link">Dashboard</Link></li>
-                <li><Link to="/profile" onClick={toggleSidebar} className="sidebar-link">Profile</Link></li>
-                <li><Link to="/community" onClick={toggleSidebar} className="sidebar-link">Community</Link></li>
-                <li><Link to="/marketplace" onClick={toggleSidebar} className="sidebar-link">Marketplace</Link></li>
-                <li><Link to="/internships" onClick={toggleSidebar} className="sidebar-link">Internships</Link></li>
-                <li><Link to="/help-center" onClick={toggleSidebar} className="sidebar-link">Help Center</Link></li>
-            </ul>
-        </div>
+                <div className="sidebar-header">
+                    <h2 className="sidebar-title">Menu</h2>
+                    <button className='close-btn' onClick={toggleSidebar}>
+                        <i className="fa fa-times"></i>
+                    </button>
+                </div>
+                <ul className='sidebar-menu'>
+                    <li>
+                        <Link to="/home" onClick={toggleSidebar} className="sidebar-link">
+                            <i className="fa fa-home"></i>
+                            <span>Dashboard</span>
+                        </Link>
+                    </li>
+                    <li>
+                        <Link to="/profile" onClick={toggleSidebar} className="sidebar-link">
+                            <i className="fa fa-user"></i>
+                            <span>Profile</span>
+                        </Link>
+                    </li>
+                    <li>
+                        <Link to="/community" onClick={toggleSidebar} className="sidebar-link">
+                            <i className="fa fa-users"></i>
+                            <span>Community</span>
+                        </Link>
+                    </li>
+                    <li>
+                        <Link to="/marketplace" onClick={toggleSidebar} className="sidebar-link">
+                            <i className="fa fa-shopping-cart"></i>
+                            <span>Marketplace</span>
+                        </Link>
+                    </li>
+                    <li>
+                        <Link to="/internships" onClick={toggleSidebar} className="sidebar-link">
+                            <i className="fa fa-graduation-cap"></i>
+                            <span>Internships</span>
+                        </Link>
+                    </li>
+                    <li>
+                        <Link to="/help-center" onClick={toggleSidebar} className="sidebar-link">
+                            <i className="fa fa-question-circle"></i>
+                            <span>Help Center</span>
+                        </Link>
+                    </li>
+                </ul>
+            </div>
         </>
-    )
-}
+    );
+};
 
 export default Sidebar;
