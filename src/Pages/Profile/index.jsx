7ecc096377e2e16e3f8017443a8dea9b112cd6e5
Ali-Hadi-Saker <alihady530@gmail.com>
2025-04-05 14:47:29 +0300

    added function to get user data in profile page

--- src/Pages/Profile/index.jsx
+++ src/Pages/Profile/index.jsx
@@ -1,4 +1,14 @@
+import { useEffect, useState } from "react";
+import { getUser } from "../../utils/auth";
+
 const Profile = ()=>{
+    const [user, setUser] = useState(null);
+    useEffect(() => {
+        const storedUser = getUser();
+        setUser(storedUser);
+        console.log(storedUser);
+    }, []);
+
     return(
         <div>
             <h1>Profile</h1>
