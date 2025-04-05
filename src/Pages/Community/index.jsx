378891b4b48b661089cb2ff2ec89cb1b839035e5
Ali-Hadi-Saker <alihady530@gmail.com>
2025-04-05 20:48:10 +0300

    added fetched communities function

--- src/Pages/Community/index.jsx
+++ src/Pages/Community/index.jsx
@@ -1,4 +1,26 @@
+import { useEffect, useState } from "react";
+import { getAuthHeader } from "../../utils/auth";
+
 const Community = ()=> {
+    const [communities, setCommunities] = useState([]);
+    useEffect(() => {
+        fetchedCommunities()
+    }, []);
+    const fetchedCommunities = async () => {
+        try {
+            const response = await fetch('http://localhost:5000/api/communities', {
+                headers: {
+                    ...getAuthHeader()
+                }
+            })
+            const data = await response.json();
+            setCommunities(data);
+            console.log(data);
+        } catch (error) {
+            console.error(error);
+        }
+    }
+
     return(
         <div>
             <h1>Community</h1>
