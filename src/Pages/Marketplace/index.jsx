f30219e70fba18c2076898425b1163007b5d2403
Ali-Hadi-Saker <alihady530@gmail.com>
2025-04-07 11:36:46 +0300

    added function to fetch markeplaces

--- src/Pages/Marketplace/index.jsx
+++ src/Pages/Marketplace/index.jsx
@@ -1,4 +1,19 @@
+import { useEffect } from "react";
+
 const Marketplace = ()=> {
+    useEffect(() => {
+        fetchMarketPlace()
+    }, [])
+
+    const fetchMarketPlace = async () => {
+        try {
+            const response = await fetch('http://localhost:5000/api/marketplace');
+            const data = await response.json();
+            console.log(data);
+        } catch (error) {
+            console.error(error);
+        }
+    }
     return(
         <div>
             <h1>Marketplace</h1>
