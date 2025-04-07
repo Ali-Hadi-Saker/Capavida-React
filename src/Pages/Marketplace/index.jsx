d49357108ebe986d7fe4074165e98f8ce3d566d6
Ali-Hadi-Saker <alihady530@gmail.com>
2025-04-07 12:07:19 +0300

    added findMatchedMarketplace function

--- src/Pages/Marketplace/index.jsx
+++ src/Pages/Marketplace/index.jsx
@@ -1,6 +1,9 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
+import {getUser} from "../../utils/auth.js";
 
 const Marketplace = ()=> {
+const [matchedMarketPlace, setMatchedMarketPlace] = useState([])
+
     useEffect(() => {
         fetchMarketPlace()
     }, [])
@@ -10,9 +13,25 @@ const Marketplace = ()=> {
             const response = await fetch('http://localhost:5000/api/marketplace');
             const data = await response.json();
             console.log(data);
+            findMatchedMarketplace(data.items);
         } catch (error) {
             console.error(error);
         }
+    }
+
+    const findMatchedMarketplace = (marketplacesData) => {
+        const user = getUser();
+
+        const userDisabilities = user.disabilityTypes;
+
+        const matched = marketplacesData.filter(marketplace => 
+            marketplace.disabilityType.some(disability =>
+                userDisabilities.includes(disability)
+            )
+        );
+        setMatchedMarketPlace(matched);
+        console.log(matched);
+                
     }
     return(
         <div>
